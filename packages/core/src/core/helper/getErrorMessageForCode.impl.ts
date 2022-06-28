3e637799100d48ec170d7c8ca8252c49c9649609
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2022-06-28 19:17:33 +0200

    chore: make getErrorMessageForCode generic

--- packages/core/src/core/helper/getErrorMessageForCode.impl.ts
+++ packages/core/src/core/helper/getErrorMessageForCode.impl.ts
@@ -1,26 +1,18 @@
 import { StatusCode } from '../types'
 
 export const getErrorMessageForCode = (code: StatusCode): string => {
-  switch (code) {
-    case StatusCode.BadRequest:
-      return 'Bad Request'
-    case StatusCode.Unauthorized:
-      return 'Unauthorized'
-    case StatusCode.PaymentRequired:
-      return 'Payment Required'
-    case StatusCode.Forbidden:
-      return 'Forbidden'
-    case StatusCode.NotFound:
-      return 'Not Found'
-    case StatusCode.MethodNotAllowed:
-      return 'Method Not Allowed'
-    case StatusCode.InternalServerError:
-      return 'Internal Server Error'
-    case StatusCode.NotImplemented:
-      return 'Not Implemented'
-    case StatusCode.GatewayTimeout:
-      return 'Timeout'
-    default:
-      return 'Unspecified Error'
+  const entry = StatusCode[code]
+
+  if (!entry) {
+    return 'Unknown Error'
   }
+
+  const capitalizeFirstLetter = (input: string) => {
+    return input.charAt(0).toUpperCase() + input.slice(1)
+  }
+
+  const transform = (input: string) =>
+    input.replace(/[A-Z]+(?![a-z])|[A-Z]/g, ($, ofs) => (ofs ? ' ' : '') + capitalizeFirstLetter($))
+
+  return transform(StatusCode[code])
 }
