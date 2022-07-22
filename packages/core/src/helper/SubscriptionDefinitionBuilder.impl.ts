b2f84ffa6d6f0df460a8c91c9bda00a1cbf378b5
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2022-07-22 17:59:41 +0200

    doc: add inline documentation

--- packages/core/src/helper/SubscriptionDefinitionBuilder.impl.ts
+++ packages/core/src/helper/SubscriptionDefinitionBuilder.impl.ts
@@ -1,5 +1,11 @@
 import type { EBMessage, EBMessageType, ServiceClass, SubscriptionDefinition, SubscriptionFunction } from '../core'
 
+/**
+ * Subscription definition builder is a helper to create and define a subscriptions for a service.
+ * It helps to set all needed filters.
+ *
+ * A working schema definition needs at least a subscription name, a short description and the subscription implementation.
+ */
 export class SubscriptionDefinitionBuilder<
   ServiceClassType = ServiceClass,
   MessageType = EBMessage,
@@ -28,11 +34,33 @@ export class SubscriptionDefinitionBuilder<
     private fn: SubscriptionFunction<ServiceClassType, MessageType, Payload>,
   ) {}
 
+  /**
+   * Add a filter to only subscribe to messages with matching event name
+   * @param eventName Event name
+   * @returns SubscriptionDefinitionBuilder
+   */
   subscribeToEvent(eventName: string) {
     this.eventName = eventName
     return this
   }
 
+  /**
+   * Add filter to only match messages send by given service function & version.
+   * Set one or more parameters to undefined means "do not filter by this criteria".
+   * For example:
+   *
+   * This will filter for all messages send from function testFunction of service UserService.
+   * This will include messages from all versions of this function.
+   *
+   * ```typescript
+   * sendFrom('UserService', undefined, 'testFunction')
+   * ```
+   *
+   * @param serviceName
+   * @param serviceVersion
+   * @param serviceTarget
+   * @returns
+   */
   sendFrom(serviceName: string | undefined, serviceVersion: string | undefined, serviceTarget: string | undefined) {
     this.sender = {
       serviceName,
@@ -42,6 +70,23 @@ export class SubscriptionDefinitionBuilder<
     return this
   }
 
+  /**
+   * Add filter to only match messages received by given service function & version.
+   * Set one or more parameters to undefined means "do not filter by this criteria".
+   * For example:
+   *
+   * This will filter for all messages send to function testFunction of service UserService.
+   * This will include messages from all versions of this function.
+   *
+   * ```typescript
+   * receivedBy('UserService', undefined, 'testFunction')
+   * ```
+   *
+   * @param serviceName
+   * @param serviceVersion
+   * @param serviceTarget
+   * @returns
+   */
   receivedBy(serviceName: string | undefined, serviceVersion: string | undefined, serviceTarget: string | undefined) {
     this.receiver = {
       serviceName,
@@ -51,12 +96,26 @@ export class SubscriptionDefinitionBuilder<
     return this
   }
 
+  /**
+   * Adds a filter to match specific message type.
+   *
+   * Common message types are `Command`, `CommandSuccessResponse` and `CommandErrorResponse`.
+   *
+   * See @enum EBMessageType for full available list.
+   *
+   * @param messageType
+   * @returns
+   */
   addMessageType(messageType: EBMessageType) {
     this.messageType = messageType
 
     return this
   }
 
+  /**
+   * Returns the final subscription definition which will be passed into the service class.
+   * @returns SubscriptionDefinition
+   */
   getDefinition(): SubscriptionDefinition<ServiceClassType, MessageType, Payload> {
     const subscription: SubscriptionDefinition<ServiceClassType, MessageType, Payload> = {
       sender: this.sender,
