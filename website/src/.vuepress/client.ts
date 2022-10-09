a1b895891a53cb634fdfa129572fa5ce8f09608f
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2022-10-09 13:30:46 +0100

    fix: correct documentation generation

--- website/src/.vuepress/client.ts
+++ website/src/.vuepress/client.ts
@@ -2,22 +2,24 @@ import { defineClientConfig } from '@vuepress/client'
 
 export default defineClientConfig({
   setup() {
-    const css = document.createElement('link')
-    css.href = '/cookieconsent.css'
-    css.rel = 'stylesheet'
-    css.type = 'text/css'
-    css.media = 'all'
-    document.head.appendChild(css)
+    if (!__VUEPRESS_SSR__) {
+      const css = document.createElement('link')
+      css.href = '/cookieconsent.css'
+      css.rel = 'stylesheet'
+      css.type = 'text/css'
+      css.media = 'all'
+      document.head.appendChild(css)
 
-    const script = document.createElement('script')
-    script.onload = function () {
-      const scriptinit = document.createElement('script')
-      scriptinit.src = '/cookieconsent-init.js'
-      document.body.appendChild(scriptinit)
+      const script = document.createElement('script')
+      script.onload = function () {
+        const scriptinit = document.createElement('script')
+        scriptinit.src = '/cookieconsent-init.js'
+        document.body.appendChild(scriptinit)
+      }
+      script.src = '/cookieconsent.js'
+
+      document.body.appendChild(script)
     }
-    script.src = '/cookieconsent.js'
-
-    document.body.appendChild(script)
   },
   rootComponents: [],
 })
