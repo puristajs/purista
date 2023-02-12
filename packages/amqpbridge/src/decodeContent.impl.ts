5b4b83dc78003ecd563015b490f5f85ca4f01581
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-02-12 21:27:00 +0000

    feat: implement OpenTelemetry into AmqpBridge resolve #63

---
+++ packages/amqpbridge/src/decodeContent.impl.ts
@@ -0,0 +1,22 @@
+import { Encoder, Encrypter } from './types'
+
+export const decodeContent = async <T>(
+  input: Buffer,
+  contentType: string,
+  contentEncoding: string,
+  encrypter: Encrypter,
+  encoder: Encoder,
+): Promise<T> => {
+  const decrypter = encrypter[contentEncoding]
+  if (!decrypter) {
+    throw new Error(`Decrypt not defined for ${contentEncoding}`)
+  }
+
+  const decrypted = await decrypter.decrypt(input)
+
+  const decoder = encoder[contentType]
+  if (!decoder) {
+    throw new Error(`Decode not defined for ${contentType}`)
+  }
+  return decoder.decode(decrypted)
+}
---
+++ packages/amqpbridge/src/serializeOtpForAmqpHeader.impl.ts
@@ -0,0 +1,6 @@
+import { context, propagation } from '@opentelemetry/api'
+
+export const serializeOtpForAmqpHeader = (header: Record<string, string | undefined>) => {
+  propagation.inject(context.active(), header)
+  return header
+}
