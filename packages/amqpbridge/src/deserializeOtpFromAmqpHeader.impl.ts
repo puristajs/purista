4bca53335f6a8f39190501085a8b1ff65e95d37e
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-02-12 21:27:00 +0000

    feat: implement OpenTelemetry into AmqpBridge resolve #63

---
+++ packages/amqpbridge/src/deserializeOtpFromAmqpHeader.impl.ts
@@ -0,0 +1,37 @@
+import { context, propagation } from '@opentelemetry/api'
+import { deserializeOtp, EBMessage, Logger } from '@purista/core'
+import { ConsumeMessage } from 'amqplib'
+
+import { decodeContent } from './decodeContent.impl'
+import { Encoder, Encrypter } from './types'
+
+export const deserializeOtpFromAmqpHeader = async (
+  logger: Logger,
+  message: ConsumeMessage | null,
+  encrypter: Encrypter,
+  encoder: Encoder,
+) => {
+  if (!message) {
+    return
+  }
+  try {
+    // try to use amqp header first
+    const header = message.properties.headers
+
+    // if not present try to ffind in message content
+    if (!header['traceparent']) {
+      const msg = await decodeContent<EBMessage>(
+        message.content,
+        message.properties.contentType,
+        message.properties.contentEncoding,
+        encrypter,
+        encoder,
+      )
+      return await deserializeOtp(logger, msg.otp)
+    }
+
+    return propagation.extract(context.active(), header)
+  } catch (err) {
+    logger.error({ err }, 'unable to deserialize otp entry from amqp header')
+  }
+}
