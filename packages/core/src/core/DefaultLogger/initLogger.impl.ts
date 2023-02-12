6f46d071ff2ff02017ea3e5fced72ba875117531
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-02-12 15:56:33 +0000

    feat: simplify init logger - set default instead of required property

--- packages/core/src/core/DefaultLogger/initLogger.impl.ts
+++ packages/core/src/core/DefaultLogger/initLogger.impl.ts
@@ -2,12 +2,14 @@ import pino, { LoggerOptions } from 'pino'
 
 import type { Logger, LogLevelName } from '../types'
 import { DefaultLogger } from './DefaultLogger.impl'
+import { getDefaultLogLevel } from './getDefaultLogLevel'
 
 /**
  * Create a new logger with the given minimum log level
  * @param {LogLevelName | undefined} minLevel - The minimum level of log messages to display.
+ * @param {LoggerOptions}
  */
-export const initLogger = (level: LogLevelName | undefined, opt?: LoggerOptions): Logger => {
+export const initLogger = (level: LogLevelName = getDefaultLogLevel(), opt?: LoggerOptions): Logger => {
   return new DefaultLogger(
     pino({
       name: 'PURISTA',
