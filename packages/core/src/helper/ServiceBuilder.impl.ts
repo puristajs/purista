6e332c6c7688f97be022d659a633598fa941e707
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-02-16 21:59:08 +0000

    chore: add inline documentation

--- packages/core/src/helper/ServiceBuilder.impl.ts
+++ packages/core/src/helper/ServiceBuilder.impl.ts
@@ -16,6 +16,12 @@ import { SubscriptionDefinitionBuilder } from './SubscriptionDefinitionBuilder.i
 
 export type Newable<T> = { new (...args: any[]): T }
 
+/**
+ * This class is used to build a service.
+ * The `ServiceBuilder` class is used to build a service. It has a few methods that are used to add
+ * command definitions and subscription definitions to the service. It also has a method that is used
+ * to create an instance of the service class
+ */
 export class ServiceBuilder<
   ConfigType = Record<string, unknown>,
   ConfigInputType = Record<string, unknown>,
@@ -34,11 +40,23 @@ export class ServiceBuilder<
   // eslint-disable-next-line no-useless-constructor
   constructor(public info: ServiceInfoType) {}
 
+  /**
+   * "This function sets the config schema for the service builder."
+   *
+   * @param schema - The schema that will be used to validate the config.
+   * @returns The ServiceBuilder<O, I, Service<O>>
+   */
   setConfigSchema<I = unknown, D extends z.ZodTypeDef = z.ZodTypeDef, O = unknown>(schema: z.ZodType<O, D, I>) {
     this.configSchema = schema
     return this as unknown as ServiceBuilder<O, I, Service<O>>
   }
 
+  /**
+   * "This function sets the default configuration for the service."
+   *
+   * @param {ConfigType} config - ConfigType - The default configuration for the service.
+   * @returns The ServiceBuilder instance.
+   */
   setDefaultConfig(config: ConfigType): ServiceBuilder<ConfigType, ConfigInputType, ServiceClassType> {
     let conf = config
     if (this.configSchema) {
@@ -56,6 +74,11 @@ export class ServiceBuilder<
     return this.addCommandDefinition(...functions)
   }
 
+  /**
+   * `addCommandDefinition` adds a list of command definitions to the service builder
+   * @param commands - CommandDefinitionList<ServiceClassType>
+   * @returns The service builder
+   */
   addCommandDefinition(...commands: CommandDefinitionList<ServiceClassType>) {
     const existing = commands.filter((fn) =>
       this.commandFunctions.some((definition) => definition.commandName === fn.commandName),
@@ -74,6 +97,11 @@ export class ServiceBuilder<
     return this as ServiceBuilder<ConfigType, ConfigInputType, ServiceClassType>
   }
 
+  /**
+   * It adds a subscription definition to the service builder
+   * @param subscription - SubscriptionDefinitionList<ServiceClassType>
+   * @returns The service builder
+   */
   addSubscriptionDefinition(...subscription: SubscriptionDefinitionList<ServiceClassType>) {
     const existing = subscription.filter((fn) =>
       this.subscriptionList.some((definition) => definition.subscriptionName === fn.subscriptionName),
@@ -92,11 +120,24 @@ export class ServiceBuilder<
     return this as ServiceBuilder<ConfigType, ConfigInputType, ServiceClassType>
   }
 
+  /**
+   * It sets the class type of the service.
+   * @param c - Newable<T>
+   * @returns The builder itself, but with the type of the service class changed.
+   */
   setCustomClass<T extends ServiceClass<ConfigType>>(c: Newable<T>) {
     this.SClass = c
     return this as unknown as ServiceBuilder<ConfigType, ConfigInputType, T>
   }
 
+  /**
+   * It creates a new instance of the service class, passing in the logger, service info, event bridge,
+   * command functions, subscription list, and configuration
+   * @param {EventBridge} eventBridge - EventBridge
+   * @param options - { serviceConfig?: ConfigInputType; logger?: Logger; spanProcessor?: SpanProcessor
+   * } = {}
+   * @returns The instance of the service class
+   */
   getInstance(
     eventBridge: EventBridge,
     options: { serviceConfig?: ConfigInputType; logger?: Logger; spanProcessor?: SpanProcessor } = {},
@@ -142,6 +183,15 @@ export class ServiceBuilder<
     return new CommandDefinitionBuilder<ServiceClassType>(commandName, description, eventName)
   }
 
+  /**
+   * It returns a new instance of the CommandDefinitionBuilder class, which is a class that is used to
+   * build a command definition
+   * @param {string} commandName - The name of the command.
+   * @param {string} description - The description of the command.
+   * @param {string} [eventName] - The name of the event that will be emitted when the command is
+   * executed.
+   * @returns A CommandDefinitionBuilder object.
+   */
   getCommandBuilder(
     commandName: string,
     description: string,
@@ -150,6 +200,13 @@ export class ServiceBuilder<
     return new CommandDefinitionBuilder<ServiceClassType>(commandName, description, eventName)
   }
 
+  /**
+   * It returns a new instance of the `SubscriptionDefinitionBuilder` class, which is a class that is
+   * used to build a subscription definition
+   * @param {string} subscriptionName - The name of the subscription.
+   * @param {string} description - The description of the subscription.
+   * @returns A SubscriptionDefinitionBuilder<ServiceClassType>
+   */
   getSubscriptionBuilder(
     subscriptionName: string,
     description: string,
