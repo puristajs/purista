098ad2810aae8976e582e1ca633d57989e6f417f
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-07 18:12:44 +0000

    chore: Add unit tests #2

---
+++ packages/core/src/helper/getSubscriptionFunctionWithValidation.test.ts
@@ -0,0 +1,6 @@
+describe('getSubscriptionFunctionWithValidation', () => {
+  it.todo('throws if parameter schema validation fails')
+  it.todo('throws if input schema validation fails')
+  it.todo('returns output if no schema is defined')
+  it.todo('throws if output schema validation fails')
+})
