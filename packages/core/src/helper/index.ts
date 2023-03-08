f490051337784c3195d2082ba23375c1f0b57483
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-08 17:04:43 +0000

    breaking change: remove deprecated FunctionDefinitionBuilder #66

--- packages/core/src/helper/index.ts
+++ packages/core/src/helper/index.ts
@@ -1,7 +1,4 @@
 export * from './CommandDefinitionBuilder.impl'
-
-/** @deprecated use setCommandFunction of CommandDefinitionBuilder instead. It will be removed soon. */
-export * from './FunctionDefinitionBuilder.impl'
 export * from './getCommandFunctionWithValidation.impl'
 export * from './getSubscriptionFunctionWithValidation.impl'
 export * from './ServiceBuilder.impl'
