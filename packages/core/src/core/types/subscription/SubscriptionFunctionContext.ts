85c937b3d9a0d813b2dbaec23ff5c1414609d7f2
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-10 18:51:43 +0000

    chore: add inline doc

--- packages/core/src/core/types/subscription/SubscriptionFunctionContext.ts
+++ packages/core/src/core/types/subscription/SubscriptionFunctionContext.ts
@@ -11,7 +11,7 @@ import { StateGetterFunction, StateSetterFunction } from '../stateStore'
  * The subscription function context which will be passed into subscription function.
  */
 export type SubscriptionFunctionContext = {
-  /** the logger */
+  /** the logger instance */
   logger: Logger
   /** the original message */
   message: Readonly<EBMessage>
@@ -32,10 +32,16 @@ export type SubscriptionFunctionContext = {
     context: Context | undefined,
     fn: (span: Span) => Promise<F>,
   ) => Promise<F>
+  /** get a secret from the secret store */
   getSecret: SecretGetterFunction
+  /** set a secret in the secret store */
   setSecret: SecretSetterFunction
+  /** get a config value from the config store */
   getConfig: ConfigGetterFunction
+  /** set a config value in the config store */
   setConfig: ConfigSetterFunction
+  /** get a state value from the state store */
   getState: StateGetterFunction
+  /** set a state value in the state store */
   setState: StateSetterFunction
 }
