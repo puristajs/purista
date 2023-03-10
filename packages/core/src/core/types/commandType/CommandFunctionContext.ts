f44d4a6bb6df1a0b9084d0b3a42444a0b075ab1b
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-10 18:51:43 +0000

    chore: add inline doc

--- packages/core/src/core/types/commandType/CommandFunctionContext.ts
+++ packages/core/src/core/types/commandType/CommandFunctionContext.ts
@@ -7,26 +7,41 @@ import { SecretGetterFunction, SecretSetterFunction } from '../secretStore'
 import { StateGetterFunction, StateSetterFunction } from '../stateStore'
 import type { Command } from './Command'
 
+/**
+ * The command function context which will be passed into command function.
+ */
 export type CommandFunctionContext<MessagePayloadType = unknown, MessageParamsType = unknown> = {
+  /** the logger instance */
   logger: Logger
+  /** the original message */
   message: Readonly<Command<MessagePayloadType, MessageParamsType>>
+  /** emit a custom message */
   emit: <Payload = unknown>(eventName: string, payload?: Payload) => Promise<void>
+  /** call a other command and return the result */
   invoke: <InvokeResponseType = unknown, PayloadType = unknown, ParameterType = unknown>(
     address: EBMessageAddress,
     payload: PayloadType,
     parameter: ParameterType,
   ) => Promise<InvokeResponseType>
+  /** wrap given function in an opentelemetry span */
   wrapInSpan: <F>(name: string, opts: SpanOptions, fn: (span: Span) => Promise<F>, context?: Context) => Promise<F>
+  /** wrap given function in an opentelemetry active span */
   startActiveSpan: <F>(
     name: string,
     opts: SpanOptions,
     context: Context | undefined,
     fn: (span: Span) => Promise<F>,
   ) => Promise<F>
+  /** get a secret from the secret store */
   getSecret: SecretGetterFunction
+  /** set a secret in the secret store */
   setSecret: SecretSetterFunction
+  /** get a config value from the config store */
   getConfig: ConfigGetterFunction
+  /** set a config value in the config store */
   setConfig: ConfigSetterFunction
+  /** get a state value from the state store */
   getState: StateGetterFunction
+  /** set a state value in the state store */
   setState: StateSetterFunction
 }
--- packages/core/src/core/types/commandType/CommandTransformFunctionContext.ts
+++ packages/core/src/core/types/commandType/CommandTransformFunctionContext.ts
@@ -8,18 +8,27 @@ import { Command } from './Command'
 
 export type CommandTransformFunctionContext<PayloadType, ParameterType> = {
   logger: Logger
+  /** the original message */
   message: Readonly<Command<PayloadType, ParameterType>>
+  /** wrap given function in an opentelemetry span */
   wrapInSpan: <F>(name: string, opts: SpanOptions, fn: (span: Span) => Promise<F>, context?: Context) => Promise<F>
+  /** wrap given function in an opentelemetry active span */
   startActiveSpan: <F>(
     name: string,
     opts: SpanOptions,
     context: Context | undefined,
     fn: (span: Span) => Promise<F>,
   ) => Promise<F>
+  /** get a secret from the secret store */
   getSecret: SecretGetterFunction
+  /** set a secret in the secret store */
   setSecret: SecretSetterFunction
+  /** get a config value from the config store */
   getConfig: ConfigGetterFunction
+  /** set a config value in the config store */
   setConfig: ConfigSetterFunction
+  /** get a state value from the state store */
   getState: StateGetterFunction
+  /** set a state value in the state store */
   setState: StateSetterFunction
 }
