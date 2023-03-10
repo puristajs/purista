ef2c9718c41e539a3c028eec7953d4decaa604fe
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-10 16:13:42 +0000

    feat: improve secret store #88

--- packages/core/src/core/DefaultSecretStore/DefaultSecretStore.impl.ts
+++ packages/core/src/core/DefaultSecretStore/DefaultSecretStore.impl.ts
@@ -5,9 +5,14 @@ import { Logger, SecretStore, StatusCode } from '../types'
 import { SecretStoreBaseClass } from './SecretStoreBaseClass.impl'
 import { DefaultSecretStoreConfig } from './types'
 
+/**
+ * The DefaultSecretStore is a placeholder which offers all needed methods.
+ * There is no actual implementation of storing or fetching secrets.
+ * Getters and setters will throw a UnhandledError with status `Not implemented`
+ */
 export class DefaultSecretStore extends SecretStoreBaseClass<DefaultSecretStoreConfig> implements SecretStore {
-  constructor(config?: DefaultSecretStoreConfig, options?: { logger?: Logger; spanProcessor?: SpanProcessor }) {
-    super('DefaultSecretStore', config, options)
+  constructor(options?: { logger?: Logger; spanProcessor?: SpanProcessor }) {
+    super('DefaultSecretStore', {}, options)
     this.healthy = true
     this.started = true
   }
