7f20828b1166c98c3930ba35f21844c473ae68c6
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-10 16:13:42 +0000

    feat: improve secret store #88

---
+++ packages/core/src/core/DefaultSecretStore/defaultSecretStore.test.ts
@@ -0,0 +1,29 @@
+import { createSandbox } from 'sinon'
+
+import { getLoggerMock } from '../../testhelper'
+import { DefaultSecretStore } from './DefaultSecretStore.impl'
+
+describe('DefaultSecretStore', () => {
+  const sandbox = createSandbox()
+
+  afterEach(() => {
+    sandbox.restore()
+  })
+
+  it('can start and stop and throws on getter and setter', async () => {
+    const logger = getLoggerMock(sandbox)
+    const store = new DefaultSecretStore({ logger: logger.mock })
+
+    await expect(store.start()).resolves.toBeUndefined()
+
+    await expect(store.getSecret('example')).rejects.toThrow('getSecret is not implemented in DefaultSecretStore')
+
+    await expect(store.setSecret('example', 'value')).rejects.toThrow(
+      'setSecret is not implemented in DefaultSecretStore',
+    )
+
+    await expect(store.isHealthy()).resolves.toBeTruthy()
+
+    await expect(store.destroy()).resolves.toBeUndefined()
+  })
+})
--- packages/core/src/core/types/secretStore/SecretStore.ts
+++ packages/core/src/core/types/secretStore/SecretStore.ts
@@ -2,12 +2,39 @@ import { SecretGetterFunction } from './SecretGetterFunction'
 import { SecretSetterFunction } from './SecretSetterFunction'
 
 export interface SecretStore {
+  /**
+   * indicates if the secret store has been started and is connected to persistance
+   */
   isReady(): Promise<boolean>
 
+  /**
+   * indicates if the secret store persistance is reachable
+   */
   isHealthy(): Promise<boolean>
+
+  /**
+   * connects the secret store to persistance
+   */
   start(): Promise<void>
+
+  /**
+   * get a secret
+   * @param string name of secret
+   * @returns the secret
+   * @throws UnhandledError
+   */
   getSecret: SecretGetterFunction
+
+  /**
+   * get a secret
+   * @param string name of secret
+   * @param value value of secret
+   * @throws UnhandledError
+   */
   setSecret: SecretSetterFunction
 
+  /**
+   * disconnects and shuts down the secret store
+   */
   destroy(): Promise<void>
 }
