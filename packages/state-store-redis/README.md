88244fda386e83692c63b65c805973e3d3842430
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-27 11:46:17 +0100

    fix: redis state store and add integration test

--- packages/state-store-redis/README.md
+++ packages/state-store-redis/README.md
@@ -1,5 +1,29 @@
 # @purista/redis-state-store
 
-Package for using a redis database as state store.
+ A state store for using redis as storage.  
+ State values are stored as stringified JSON in the redis database.
+
+ By default `get
+
+```typescript
+ const config = {
+  url: 'redis://alice:foobared@awesome.redis.server:6380'
+ }
+
+ const store = new RedisStateStore({ config })
+
+ await store.setState('stateKey',{ myState: 'value' })
+
+ let value = await store.getState('stateKey')
+ console.log(value) // outputs: { myState: 'value' }
+
+ await store.removeState('stateKey')
+
+ value = await store.getState('stateKey')
+ console.log(value) // outputs: undefined
+
+ ```
+
+ See documentation of underlaying redis lib package for detailed configuration options - [NODE-REDIS](https://redis.js.org).
 
 **Visit [purista.dev](https://purista.dev)**
--- packages/state-store-redis/src/index.ts
+++ packages/state-store-redis/src/index.ts
@@ -1,5 +1,33 @@
 /**
+ * A state store for using redis as storage.
+ * State values are stored as stringified JSON.
  *
- * @module
+ * Per default, setting/changing and removal of values are enabled.
+ *
+ * @example ```typescript
+ * const config = {
+ *  enableGet: true, // optional, default is true
+ *  enableRemove: true, // optional, default is true
+ *  enableSet: true, // optional, default is true
+ *  url: 'redis://alice:foobared@awesome.redis.server:6379'
+ * }
+ *
+ * const store = new RedisStateStore({ config })
+ *
+ * await store.setState('stateKey',{ myState: 'value' })
+ *
+ * let value = await store.getState('stateKey')
+ * console.log(value) // outputs: { myState: 'value' }
+ *
+ * await store.removeState('stateKey')
+ *
+ * value = await store.getState('stateKey')
+ * console.log(value) // outputs: undefined
+ * ```
+ *
+ * See documentation of underlaying redis lib package for detailed configuration options.
+ *
+ * @see [NODE-REDIS](https://redis.js.org)
+ *
  */
 export * from './RedisStateStore.impl'
