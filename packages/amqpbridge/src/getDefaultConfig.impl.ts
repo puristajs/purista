6a8d4b82afce8e04a70d1458ec2a00baedd310a9
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-27 19:51:30 +0100

    chore: improve types

--- packages/amqpbridge/src/getDefaultConfig.impl.ts
+++ packages/amqpbridge/src/getDefaultConfig.impl.ts
@@ -1,11 +1,15 @@
+import { Complete } from '@purista/core'
+
 import { AmqpBridgeConfig } from './types'
 
-export const getDefaultConfig = (): AmqpBridgeConfig => {
+export const getDefaultConfig = (): Complete<AmqpBridgeConfig> & { exchangeName: string; url: string } => {
   return {
     exchangeName: 'purista',
     namePrefix: 'purista',
     url: 'amqp://localhost',
     encoder: {},
     encrypter: {},
+    socketOptions: undefined,
+    exchangeOptions: undefined,
   }
 }
