85168abf3ff3781004d012173b4c5571caa1959b
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-03-31 11:35:41 +0100

    feat: add helper and cleanup code

---
+++ packages/core/src/helper/getTimeoutPromise.impl.ts
@@ -0,0 +1,20 @@
+import { StatusCode, UnhandledError } from '../core'
+
+/**
+ *
+ * @param fn the promise which should get a timeout
+ * @param ttl the timeout in ms @default 30000
+ * @returns
+ */
+export const getTimeoutPromise = <T>(fn: Promise<T>, ttl = 30000): Promise<T> => {
+  let timeout: ReturnType<typeof setTimeout>
+  const ttlPromise = new Promise<never>((_resolve, reject) => {
+    timeout = setTimeout(() => {
+      const err = new UnhandledError(StatusCode.GatewayTimeout, 'invocation timed out')
+      clearTimeout(timeout)
+      reject(err)
+    }, ttl)
+  })
+
+  return Promise.race([fn, ttlPromise])
+}
---
+++ packages/core/src/helper/throwIfNotValidMessage.impl.ts
@@ -0,0 +1,33 @@
+import { z, ZodError } from 'zod'
+
+import { EBMessageType, StatusCode, UnhandledError } from '../core'
+
+/**
+ * Validates if the given input might be valid event bridge message
+ * It only checks for "technically possible"
+ * @param input
+ */
+export const throwIfNotValidMessage = (input: unknown) => {
+  const ebMessageSchema = z
+    .object({
+      messageType: z.nativeEnum(EBMessageType),
+      id: z.string(),
+      instanceId: z.string(),
+      timestamp: z.number(),
+      contentType: z.string(),
+      contentEncoding: z.string(),
+      traceId: z.string().optional(),
+      correlationId: z.string().optional(),
+      principalId: z.string().optional(),
+      eventName: z.string().optional(),
+      otp: z.string().optional(),
+    })
+    .passthrough()
+
+  try {
+    ebMessageSchema.parse(input)
+  } catch (error) {
+    const err = error as ZodError
+    throw UnhandledError.fromError(err, StatusCode.BadRequest, err.issues)
+  }
+}
