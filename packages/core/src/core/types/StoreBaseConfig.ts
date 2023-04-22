9156219b2d2f64d87ae8f66377199f14d1270fdd
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-04-22 10:51:12 +0100

    doc: add inline code documentation

--- packages/core/src/core/types/StoreBaseConfig.ts
+++ packages/core/src/core/types/StoreBaseConfig.ts
@@ -1,9 +1,32 @@
 import { Logger } from './Logger'
+import { LogLevelName } from './LogLevelName'
 
+/**
+ * Basic configuration object which is used by any store
+ */
 export type StoreBaseConfig<Config> = {
+  /**
+   * Enable generally get method
+   */
   enableGet?: boolean
+  /**
+   * Enable generally set method
+   */
   enableSet?: boolean
+  /**
+   * Enable generally remove method
+   */
   enableRemove?: boolean
+  /**
+   * A logger instance
+   */
   logger?: Logger
+  /**
+   * A log level for new logger if logger is not set
+   */
+  logLevel?: LogLevelName
+  /**
+   * Custom store config
+   */
   config?: Config
 }
