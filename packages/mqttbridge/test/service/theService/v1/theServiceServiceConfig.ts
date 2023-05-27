9aa3d27361fb83b90dea8802ae955e6a5b5d76d4
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-05-27 15:11:33 +0100

    chore: share bridge integration test setup

--- packages/mqttbridge/test/service/theService/v1/theServiceServiceConfig.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'zod'
-
-// define the service config schema and the default service configuration
-
-export const theServiceServiceV1ConfigSchema = z.object({})
-
-export type TheServiceServiceV1Config = z.input<typeof theServiceServiceV1ConfigSchema>
