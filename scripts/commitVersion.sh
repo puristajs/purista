873d8592570758a97be981efbb81dfc031a1ce7b
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-06-01 13:12:11 +0100

    chore: cleanup
    Signed-off-by: Sebastian Wessel <sebastianwessel@users.noreply.github.com>

--- scripts/commitVersion.sh
+++ scripts/commitVersion.sh
@@ -10,13 +10,16 @@ echo $CONTENT > ./packages/cli/src/version.ts
 echo $CONTENT > ./packages/core/src/version.ts
 echo $CONTENT > ./packages/dapr-sdk/src/version.ts
 echo $CONTENT > ./packages/httpserver/src/version.ts
+echo $CONTENT > ./packages/infisical-secret-store/src/version.ts
 echo $CONTENT > ./packages/k8s-sdk/src/version.ts
 echo $CONTENT > ./packages/mqttbridge/src/version.ts
-echo $CONTENT > ./packages/redis-state-store/src/version.ts
-echo $CONTENT > ./packages/redis-config-store/src/version.ts
-echo $CONTENT > ./packages/nats-state-store/src/version.ts
 echo $CONTENT > ./packages/nats-config-store/src/version.ts
-echo $CONTENT > ./packages/infisical-secret-store/src/version.ts
+echo $CONTENT > ./packages/nats-state-store/src/version.ts
+echo $CONTENT > ./packages/natsbridge/src/version.ts
+echo $CONTENT > ./packages/redis-config-store/src/version.ts
+echo $CONTENT > ./packages/redis-state-store/src/version.ts
+
+
 
 git add .
 git commit -am "chore: bump versions to $NODE_VERSION" -S
