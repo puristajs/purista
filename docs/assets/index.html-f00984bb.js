464245cb583c47bde0c4931a74dc7f064c8d4014
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-06-01 13:57:47 +0100

    doc: update documentation

--- docs/assets/index.html-f00984bb.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper-c27b6911.js";import{r as t,o as n,c,e as r}from"./app-2c9217f0.js";const a={};function _(s,l){const e=t("AutoCatalog");return n(),c("div",null,[r(e)])}const i=o(a,[["render",_],["__file","index.html.vue"]]);export{i as default};
