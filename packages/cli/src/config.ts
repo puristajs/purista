bdccde9ccc29babbedcecd2e26712ee2cfa5c448
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-07-06 12:05:02 +0100

    chore: update deps in cli auto generator

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -1,27 +1,27 @@
 export const TEMPLATE_BASE = '../../blueprint'
 
-export const dependencies: string[] = ['typescript@^5.0.2', '@purista/core@latest', 'zod@3.21.4', 'ts-node@^10.9.1']
+export const dependencies: string[] = ['typescript@^5.1.6', '@purista/core@latest', 'zod@3.21.4', 'ts-node@^10.9.1']
 
 export const httpserverDependencies: string[] = ['@purista/httpserver@latest']
 
-export const httpStaticDependencies: string[] = ['@fastify/static@^6.9.0']
+export const httpStaticDependencies: string[] = ['@fastify/static@^6.10.2']
 
-export const devDependencies: string[] = ['pino-pretty@^9.1.1', '@types/node@^18.15.11']
+export const devDependencies: string[] = ['pino-pretty@^10.0.1', '@types/node@^18.16.19']
 
 export const cliDependencies = ['@purista/cli@latest']
 
 export const testDependencies: string[] = [
   '@swc/jest@^0.2.26',
-  '@types/jest@^29.5.2',
+  '@types/jest@^29.x',
   '@types/sinon@^10.x',
-  'jest@^29.5.0',
-  'sinon@^15.x',
+  'jest@^29.x',
+  'sinon@^15.2.x',
 ]
 
 export const lintDependencies: string[] = [
-  '@typescript-eslint/eslint-plugin@^5.59.8',
-  '@typescript-eslint/parser@^5.59.8',
-  'eslint@^8.41.0',
+  '@typescript-eslint/eslint-plugin@^5.61.0',
+  '@typescript-eslint/parser@^5.61.0',
+  'eslint@^8.44.0',
   'eslint-config-prettier@^8.8.0',
   'eslint-config-standard@^17.1.0',
   'eslint-plugin-import@^2.27.5',
@@ -29,5 +29,5 @@ export const lintDependencies: string[] = [
   'eslint-plugin-node@^11.1.0',
   'eslint-plugin-prettier@^4.2.1',
   'eslint-plugin-simple-import-sort@^10.0.0',
-  'prettier@^2.8.8',
+  'prettier@^3.0.0',
 ]
