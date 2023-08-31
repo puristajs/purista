0517c1a757621d9263e423845e036df7d4e107e9
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-08-31 12:25:26 +0100

    fix: openapi security

--- packages/httpserver/src/service/httpServer/v1/routes/openapi/getOpenApiJson.impl.ts
+++ packages/httpserver/src/service/httpServer/v1/routes/openapi/getOpenApiJson.impl.ts
@@ -297,7 +297,7 @@ export const getOpenApiJson = function (this: HttpServerClass<HttpServerServiceV
         ...paths[path],
         [definition.method.toLowerCase()]: {
           deprecated: expose.deprecated,
-          security: securitySchema.length > 0 && definition.openApi?.isSecure ? securitySchema : undefined,
+          security: securitySchema.length > 0 && definition.openApi?.isSecure ? securitySchema : [],
           description: definition.openApi?.description,
           summary: definition.openApi?.summary,
           parameters: [...pathParams, ...queryParams, requestIdParameter],
