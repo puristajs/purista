075d4682142dce56d55b90c531779c34e6b25b43
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-11-18 19:04:10 +0000

    feat: Add secret store for Google Cloud Secret Manager #108

---
+++ packages/gcloud-secret-store/README.md
@@ -0,0 +1,29 @@
+# @purista/infisical-secret-store
+
+A secret store for using [Infisical](https://infisical.com/) as storage.  
+
+```typescript
+const config = {
+  baseUrl: 'http://example.com'
+}
+
+const store = new InfisicalSecretStore(config)
+
+await store.setSecret('mySecret', 'value')
+
+let value = await store.getSecret('mySecret')
+console.log(value) // outputs: { mySecret: 'value' }
+
+await store.removeSecret('mySecret')
+
+value = await store.getSecret('mySecret')
+console.log(value) // outputs: undefined
+
+```
+
+**Visit [purista.dev](https://purista.dev)**
+
+**Follow on Twitter [@purista_js](https://twitter.com/purista_js)**  
+**Join the [Discord Chat](https://discord.gg/9feaUm3H2v)**
+
+<a href="https://www.producthunt.com/posts/purista?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-purista" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=386519&theme=light" alt="PURISTA - Typescript&#0032;framework&#0032;for&#0032;IoT&#0044;&#0032;microservices&#0044;&#0032;and&#0032;serverless | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
---
+++ packages/gcloud-secret-store/src/GoogleSecretStore.impl.ts
@@ -0,0 +1,106 @@
+import { join } from 'node:path/posix'
+
+import { SecretManagerServiceClient } from '@google-cloud/secret-manager'
+import { SecretStoreBaseClass, StatusCode, type StoreBaseConfig, UnhandledError } from '@purista/core'
+
+import { GoogleSecretStoreConfig } from './types'
+
+/**
+ * The secret store adapter for Google Secret Manager.
+ * It will store, retrive, update or remove secrets in Google Secret Manager.
+ *
+ * For performance reasons, and to reduce costs, the secret values are cached in memory after first fetch.
+ *
+ * You can disable the whole caching via config by setting enableCache to false.
+ * If the cache is enabled, you can set the ttl for cached secret values via config cacheTtl (in ms).
+ *
+ * This will return the cached secret if available and if ttl is not exceeded.
+ * If a secret value exceeds the ttl, it does not automatically get removed from cache.
+ * It will be removed/overwritten on next get request.
+ */
+export class GoogleSecretStore extends SecretStoreBaseClass<GoogleSecretStoreConfig> {
+  client: SecretManagerServiceClient
+
+  constructor(config: StoreBaseConfig<GoogleSecretStoreConfig>) {
+    super('GoogleSecretStore', { enableCache: true, ...config })
+    this.client = new SecretManagerServiceClient(this.config.client)
+  }
+
+  async getSecret(...secretNames: string[]): Promise<Record<string, string | undefined>> {
+    if (!this.config.enableGet) {
+      throw new UnhandledError(StatusCode.Unauthorized, 'get secret from store is disabled by config')
+    }
+
+    const result: Record<string, string | undefined> = {}
+
+    for (const name of secretNames) {
+      let value: string | undefined
+      if (this.config.enableCache) {
+        const cachedValue = this.cache.get(name)
+        if (cachedValue) {
+          if (this.config.cacheTtl !== undefined) {
+            value = cachedValue.createdAt + this.config.cacheTtl >= Date.now() ? cachedValue.value : undefined
+          } else {
+            value = cachedValue.value
+          }
+        }
+      }
+
+      if (!value) {
+        try {
+          const res = await this.client.accessSecretVersion({
+            name: join(this.config.project, 'secrets', name, 'versions', 'latest'),
+          })
+          value = res[0].payload?.data?.toString()
+        } catch (err) {
+          this.logger.error({ err })
+        }
+      }
+
+      result[name] = value
+    }
+
+    return result
+  }
+
+  async removeSecret(secretName: string) {
+    if (!this.config.enableRemove) {
+      throw new UnhandledError(StatusCode.Unauthorized, 'remove secret from store is disabled by config')
+    }
+
+    this.cache.delete(secretName)
+
+    await this.client.deleteSecret({ name: join(this.config.project, 'secrets', secretName) })
+  }
+
+  async setSecret(secretName: string, secretValue: string) {
+    if (!this.config.enableSet) {
+      throw new UnhandledError(StatusCode.Unauthorized, 'set secret at store is disabled by config')
+    }
+
+    const existingValue = await this.getSecret(secretName)
+
+    if (!existingValue[secretName]) {
+      await this.client.createSecret({
+        parent: this.config.project,
+        secretId: secretName,
+        secret: {
+          replication: {
+            automatic: {},
+          },
+        },
+      })
+    }
+
+    await this.client.addSecretVersion({
+      parent: join(this.config.project, 'secrets', secretName),
+      payload: {
+        data: Buffer.from(secretValue, 'utf8'),
+      },
+    })
+
+    if (this.config.enableCache) {
+      this.cache.set(secretName, { value: secretValue, createdAt: Date.now() })
+    }
+  }
+}
