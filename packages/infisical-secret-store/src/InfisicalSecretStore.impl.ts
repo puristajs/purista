f6fa730af752c96fd96f6dc7f5fa39f9cee470a2
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2023-11-18 20:00:48 +0000

    breaking change: Add caching to Infisical secret store and enable per default cache

--- packages/infisical-secret-store/src/InfisicalSecretStore.impl.ts
+++ packages/infisical-secret-store/src/InfisicalSecretStore.impl.ts
@@ -6,6 +6,15 @@ import { InfisicalSecretConfig } from './types'
 /**
 A secret store for using [Infisical](https://infisical.com/) as storage.  
 
+For performance reasons, and to reduce costs, the secret values are cached in memory after first fetch.
+
+You can disable the whole caching via config by setting enableCache to false.  
+If the cache is enabled, you can set the ttl for cached secret values via config cacheTtl (in ms).  
+
+This will return the cached secret if available and if ttl is not exceeded.  
+If a secret value exceeds the ttl, it does not automatically get removed from cache.  
+It will be removed/overwritten on next get request.  
+
 @example ```typescript
 const config = {
   baseUrl: 'http://example.com'
@@ -29,8 +38,7 @@ export class InfisicalSecretStore extends SecretStoreBaseClass<InfisicalSecretCo
   public client: InfisicalClient
 
   constructor(config: StoreBaseConfig<InfisicalSecretConfig>) {
-    super('InfisicalSecretStore', { ...config })
-    // this.client = new InfisicalClient(this.config)
+    super('InfisicalSecretStore', { enableCache: true, ...config })
 
     this.client = new InfisicalClient({
       name: 'InfisicalClient',
@@ -44,13 +52,26 @@ export class InfisicalSecretStore extends SecretStoreBaseClass<InfisicalSecretCo
     }
 
     const result: Record<string, string | undefined> = {}
-    for await (const name of secretNames) {
-      try {
-        result[name] = await this.client.getSecret(name)
-      } catch (err) {
-        const msg = `error in secret store getting value ${name}`
-        this.logger.error({ err }, msg)
-        throw new UnhandledError(StatusCode.InternalServerError, msg)
+    for (const name of secretNames) {
+      if (this.config.enableCache) {
+        const cachedValue = this.cache.get(name)
+        if (cachedValue) {
+          if (this.config.cacheTtl !== undefined) {
+            result[name] = cachedValue.createdAt + this.config.cacheTtl >= Date.now() ? cachedValue.value : undefined
+          } else {
+            result[name] = cachedValue.value
+          }
+        }
+      }
+
+      if (!result[name]) {
+        try {
+          result[name] = await this.client.getSecret(name)
+        } catch (err) {
+          const msg = `error in secret store getting value ${name}`
+          this.logger.error({ err }, msg)
+          throw new UnhandledError(StatusCode.InternalServerError, msg)
+        }
       }
     }
     return result
@@ -61,6 +82,8 @@ export class InfisicalSecretStore extends SecretStoreBaseClass<InfisicalSecretCo
       throw new UnhandledError(StatusCode.Unauthorized, 'remove secret from store is disabled by config')
     }
 
+    this.cache.delete(secretName)
+
     try {
       await this.client.removeSecret(secretName)
     } catch (err) {
@@ -70,13 +93,17 @@ export class InfisicalSecretStore extends SecretStoreBaseClass<InfisicalSecretCo
     }
   }
 
-  async setSecret(secretName: string, configValue: string) {
+  async setSecret(secretName: string, secretValue: string) {
     if (!this.config.enableSet) {
       throw new UnhandledError(StatusCode.Unauthorized, 'set secret at store is disabled by config')
     }
 
     try {
-      await this.client.setSecret(secretName, configValue)
+      await this.client.setSecret(secretName, secretValue)
+
+      if (this.config.enableCache) {
+        this.cache.set(secretName, { value: secretValue, createdAt: Date.now() })
+      }
     } catch (err) {
       const msg = `error in secret store setting value ${secretName}`
       this.logger.error({ err }, msg)
