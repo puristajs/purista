044e98953f511ef9934da831a2a42316ade0ae7b
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-02-09 16:30:15 +0000

    chore: improve example

--- examples/hono-example/src/service/delay/generalDelayServiceInfo.ts
+++ examples/hono-example/src/service/delay/generalDelayServiceInfo.ts
@@ -1,6 +1,6 @@
 import type { ServiceInfoType } from '@purista/core'
 
-export const generalDelayServiceInfo: Omit<ServiceInfoType, 'serviceVersion'> = {
+export const generalDelayServiceInfo = {
   serviceName: 'Delay',
   serviceDescription: 'Example to show a service which starts after webserver and registers commands',
-}
+} as const satisfies Omit<ServiceInfoType, 'serviceVersion'>
