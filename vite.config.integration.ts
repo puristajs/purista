66901d24821100cb3edb2937195f4421153bfacc
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-02-18 01:51:58 +0000

    chore: impove test setup

--- vite.config.integration.ts
+++ vite.config.integration.ts
@@ -2,16 +2,14 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    isolate: true,
     globals: true,
     watch: false,
     coverage: {
-      enabled: true,
+      enabled: false,
       include: ['**/src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     },
-    include: [
-      '**/src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-      // '**/test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
-    ],
+    include: ['**/test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**', '**/.tshy-build/**', '**/src/**'],
   },
 })
