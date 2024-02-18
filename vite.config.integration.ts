77e64e9203ee7c19ed9259c7ff052df702662408
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-02-18 01:13:14 +0000

    chore: use vitest instead of jest

---
+++ vite.config.integration.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    watch: false,
+    coverage: {
+      enabled: true,
+      include: ['**/src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    },
+    include: [
+      '**/src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+      // '**/test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+    ],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.tshy-build/**', '**/src/**'],
+  },
+})
