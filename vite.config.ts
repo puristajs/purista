5abd2ff6f7b2d27fb7ba255c3932c792c995b8a8
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-02-23 13:16:51 +0000

    chore: update workflows

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,18 @@ export default defineConfig({
     coverage: {
       enabled: true,
       include: ['**/src/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      exclude: ['examples/**', 'packages/cli/**'],
+      thresholds: {
+        lines: 63,
+        functions: 63,
+        branches: 74,
+        statements: 63,
+      },
     },
     include: [
       '**/src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
       // '**/test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
     ],
-    exclude: ['**/node_modules/**', '**/dist/**', '**/.tshy-build/**', '**/test/**'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/.tshy-build/**', '**/test/**', 'examples/**'],
   },
 })
