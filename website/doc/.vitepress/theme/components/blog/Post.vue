2be42f883bf55faaf783bbdeb6acd692d791a4db
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-02-24 20:36:33 +0000

    doc: add and improve doc

--- website/doc/.vitepress/theme/components/blog/Post.vue
+++ website/doc/.vitepress/theme/components/blog/Post.vue
@@ -1,40 +1,44 @@
-<script setup lang='ts'>
+<script setup lang="ts">
 import { useData } from 'vitepress'
 import type { Post } from '../../composables/posts.data.js'
 
 const props = defineProps<{
-    post: Post
+  post: Post
 }>()
 const { site } = useData()
+const getImgUrl = (title: string, description: string) =>
+  `https://ogpreview-ten.vercel.app/api/og?title=${encodeURIComponent(title)}&description=${encodeURIComponent(description)}`
 </script>
 
 <template>
-    <a :href="`${post.href}`" style="display: block;" :title="`${post.title}`">
-        <div>
-            <div style="display: flex;flex-direction: row;">
-                <h2 style="flex-grow: 2;">{{ post.title }}</h2>
-                <small style="text-align: right;">{{ post.date.string }}</small>
-            </div>
-            <img v-if="post.data.image" :src="`${post.data.image}`" :alt="`${post.title}`" style="margin-bottom:24px">
-            <p v-html="post.excerpt" />
-            <div class="more">
-                Read more
-            </div>
-        </div>
-    </a>
+  <a :href="`${post.href}`" style="display: block" :title="`${post.title}`">
+    <div>
+      <div style="display: flex; flex-direction: row">
+        <h2 style="flex-grow: 2">{{ post.title }}</h2>
+        <small style="text-align: right">{{ post.date.string }}</small>
+      </div>
+      <img
+        :src="`${post.data.image || getImgUrl(post.data.title, post.data.description)}`"
+        :alt="`${post.title}`"
+        style="margin-bottom: 24px"
+      />
+      <p v-html="post.excerpt" />
+      <div class="more">Read more</div>
+    </div>
+  </a>
 </template>
 
 <style scoped>
 h2 {
-    line-height: 24px;
-    font-size: 16px;
-    font-weight: 600;
-    margin-bottom: 24px;
+  line-height: 24px;
+  font-size: 16px;
+  font-weight: 600;
+  margin-bottom: 24px;
 }
 
 .more {
-    display: block;
-    margin-top: 24px;
-    text-align: right;
+  display: block;
+  margin-top: 24px;
+  text-align: right;
 }
 </style>
--- website/doc/.vitepress/theme/components/blog/Posts.vue
+++ website/doc/.vitepress/theme/components/blog/Posts.vue
@@ -2,40 +2,42 @@
 import usePosts from '../../composables/usePosts.js'
 
 const { allPosts: posts } = usePosts()
-
 </script>
 
 <template>
-    <div class="items">
-        <template v-for="post of posts">
-            <Post :post="post" class="item" />
-        </template>
-    </div>
+  <div class="items">
+    <template v-for="post of posts">
+      <Post :post="post" class="item" />
+    </template>
+  </div>
 </template>
 
 <style scoped>
 .items {
-    display: grid;
-    gap: 1rem;
-    margin: 0 auto;
-    max-width: 1152px;
+  display: grid;
+  gap: 1rem;
+  margin: 0 auto;
+  max-width: 1152px;
 }
 
 .item {
-    border: 1px solid var(--vp-c-bg-soft);
-    border-radius: 12px;
-    background-color: var(--vp-c-bg-soft);
-    transition: border-color 0.25s, background-color 0.25s;
-    padding: 24px;
+  border: 1px solid var(--vp-c-bg-soft);
+  border-radius: 12px;
+  background-color: var(--vp-c-bg-soft);
+  transition:
+    border-color 0.25s,
+    background-color 0.25s;
+  padding: 24px;
+  text-decoration: none;
 }
 
 @media (min-width: 600px) {
-    .items {
-        grid-template-columns: repeat(2, 1fr);
-    }
+  .items {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 
 .item:hover {
-    border-color: var(--vp-c-brand-1);
+  border-color: var(--vp-c-brand-1);
 }
 </style>
