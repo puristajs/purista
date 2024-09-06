dfd180fe7fcfed59bb965e220890dfbea687c42d
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2024-09-06 09:55:42 +0100

    chore: improve tests

--- packages/httpserver/src/service/httpServer/v1/subscription/serviceCommandsToRestApi/serviceCommandsToRestApi.test.ts
+++ packages/httpserver/src/service/httpServer/v1/subscription/serviceCommandsToRestApi/serviceCommandsToRestApi.test.ts
@@ -1,10 +1,4 @@
-import {
-	getCommandSuccessMessageMock,
-	getEventBridgeMock,
-	getLoggerMock,
-	getSubscriptionContextMock,
-	safeBind,
-} from '@purista/core'
+import { getCommandSuccessMessageMock, getEventBridgeMock, getLoggerMock, safeBind } from '@purista/core'
 import { createSandbox } from 'sinon'
 
 import { httpServerV1Service } from '../../httpServerV1Service.js'
@@ -43,7 +37,7 @@ describe('service HttpServer version 1 - subscription serviceCommandsToRestApi',
 		const message = getCommandSuccessMessageMock(payload)
 
 		// create a subscription context for the subscription function
-		const context = getSubscriptionContextMock(message, sandbox)
+		const context = serviceCommandsToRestApiSubscriptionBuilder.getSubscriptionContextMock(message, sandbox)
 
 		// execute the subscription function
 		const result = await serviceCommandsToRestApi(context.mock, payload, parameter)
