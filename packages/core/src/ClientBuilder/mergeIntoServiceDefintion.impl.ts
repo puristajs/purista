bc427323edbf832b6b9eaac7a16eefe6c53184ff
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2026-02-06 15:15:25 +0100

    fix(core): handle new service versions in definition merge

--- packages/core/src/ClientBuilder/mergeIntoServiceDefintion.impl.ts
+++ packages/core/src/ClientBuilder/mergeIntoServiceDefintion.impl.ts
@@ -4,9 +4,14 @@ export const mergeIntoServiceDefintion = (current: FullServiceDefinition, add: F
 	for (const [serviceName, value] of Object.entries(add)) {
 		if (current[serviceName]) {
 			for (const [serviceVersion, val] of Object.entries(value)) {
+				if (!current[serviceName][serviceVersion]) {
+					current[serviceName][serviceVersion] = val
+					continue
+				}
+
 				current[serviceName][serviceVersion] = {
 					description: current[serviceName][serviceVersion].description ?? val.description,
-					deprecated: current[serviceName][serviceVersion].deprecated,
+					deprecated: current[serviceName][serviceVersion].deprecated ?? val.deprecated,
 					commands: { ...val.commands, ...current[serviceName][serviceVersion].commands },
 					subscriptions: { ...val.subscriptions, ...current[serviceName][serviceVersion].subscriptions },
 				}
