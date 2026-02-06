bd8365e06d4690c27bd2541a6c54095662f26fdb
Sebastian Wessel <sebastianwessel@users.noreply.github.com>
2026-02-06 20:00:38 +0100

    refactor(example-quickstart): migrate deprecated httpserver to hono

--- examples/quickstart/src/index.ts
+++ examples/quickstart/src/index.ts
@@ -1,5 +1,6 @@
+import { serve } from '@hono/node-server'
 import { DefaultEventBridge } from '@purista/core'
-import { httpServerV1Service } from '@purista/httpserver'
+import { honoV1Service } from '@purista/hono-http-server'
 
 import { pingV1Service } from './service/ping/v1/index.js'
 
@@ -7,15 +8,22 @@ export const main = async () => {
 	// initiate the event bridge as first step
 	const eventBridge = new DefaultEventBridge()
 	await eventBridge.start()
-	// initiate the webserver service as second step
-	const httpServerService = await httpServerV1Service.getInstance(eventBridge)
-
-	// start the webserver
-	await httpServerService.start()
-
 	// add your service
 	const pingService = await pingV1Service.getInstance(eventBridge)
 	await pingService.start()
+
+	// initiate the webserver service as second step
+	const honoService = await honoV1Service.getInstance(eventBridge, {
+		serviceConfig: {
+			services: [pingService],
+		},
+	})
+	await honoService.start()
+
+	serve({
+		fetch: honoService.app.fetch,
+		port: 3000,
+	})
 }
 
 main()
